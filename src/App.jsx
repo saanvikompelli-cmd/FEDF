16f8cd7d773766df59323dca1d1ae995379e47ad
Saanvi <saanvi.kompelli@gmail.com>
2026-06-11 11:28:00 +0530

    Added profile section and improved home page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -10,25 +11,53 @@ import FlightDetails from './pages/FlightDetails'
 import Contact from './pages/Contact'
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [username, setUsername] = useState("Ketana")
+
   return (
     <BrowserRouter>
 
       <nav>
         <h2>Flight Status Tracker</h2>
 
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/signup">Sign Up</Link></li>
-          <li><Link to="/flights">Flights</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
+<ul>
+ <li><Link to="/">Home</Link></li>
+
+{!isLoggedIn ? (
+  <>
+    <li><Link to="/login">Login</Link></li>
+    <li><Link to="/signup">Sign Up</Link></li>
+  </>
+) : null}
+
+<li><Link to="/flights">Flights</Link></li>
+<li><Link to="/contact">Contact</Link></li>
+</ul>
+{isLoggedIn && (
+  <div className="profile">
+    👤 {username}
+
+    <button
+      className="logout-btn"
+      onClick={() => setIsLoggedIn(false)}
+    >
+      Logout
+    </button>
+  </div>
+)}
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route
+  path="/login"
+  element={<Login setIsLoggedIn={setIsLoggedIn} />}
+/>
+        <Route
+  path="/signup"
+  element={<Signup setIsLoggedIn={setIsLoggedIn} />}
+/>
         <Route path="/flights" element={<FlightSearch />} />
         <Route path="/details" element={<FlightDetails />} />
         <Route path="/contact" element={<Contact />} />
